c8161aa2314bf73394ddb215d9282d2472f734c8
Albin <alvinfer67@gmail.com>
2024-10-28 13:42:30 -0600

    update: switching to express middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import { ApolloServer } from '@apollo/server'; 
 import { startStandaloneServer } from '@apollo/server/standalone'; 
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import typeDefs from './typeDefs/index';
+import express from 'express';
+import http from 'http'
 import { userResolvers } from './resolvers/user';
 import { tableResolvers } from './resolvers/table';
 import { signedUrlResolvers } from './resolvers/signedUrl';
 import { foodResolvers } from './resolvers/food';
+
+const app = express();
+const httpServer = http.createServer(app);
 
 const resolvers = {
     Query: {
@@ -22,6 +29,20 @@ const resolvers = {
 const server = new ApolloServer({
     typeDefs: typeDefs,
     resolvers: resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 })
 
-await startStandaloneServer(server, { listen: { port: 3000 } });
+
+await server.start();
+
+app.use(
+  '/',
+  express.json(),
+  expressMiddleware(server),
+);
+
+
+await new Promise<void>((resolve) =>
+  httpServer.listen({ port: 4000 }, resolve),
+);
+console.log(`🚀 Server ready at http://localhost:4000/`);
