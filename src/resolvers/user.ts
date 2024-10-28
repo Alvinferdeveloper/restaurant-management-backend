60e631bd9810f5f0e2b816becc44c97116523be6
Albin <alvinfer67@gmail.com>
2024-10-28 16:31:52 -0600

    update: setting token in cookie whe user logged

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,4 +1,5 @@
 import prisma from "../lib/prisma";
+import { generateToken } from "../services/token.service";
 export const userResolvers = {
     Query: {
       users: ()=> {
@@ -9,10 +10,15 @@ export const userResolvers = {
         const userRole = await prisma.role.findFirst({ where: { name: "USER"}})
         return prisma.user.create({ data: { roles: { connect: { id: userRole.id}}, ...args.userInput} , include:{ roles: true}});
       },
-      login: async (root, args) => {
-        const user =  await prisma.user.findFirst({ where: { email: args.email, password: args.password}, include:{ roles:true}});
-        if(user) return user;
-        else return prisma.admin.findFirst({ where: { email: args.email, password: args.password}, include:{ roles: true}});
+      login: async (root, args, { res }) => {
+        let userLogged;
+        userLogged =  await prisma.user.findFirst({ where: { email: args.email, password: args.password}, include:{ roles:true}});
+        if(!userLogged) userLogged = prisma.admin.findFirst({ where: { email: args.email, password: args.password}, include:{ roles: true}});
+        if(userLogged){
+          const token = generateToken(userLogged);
+          res.cookie('restaurant_token',token , { httpOnly: true, maxAge: 1000 * 60 * 60 }); 
+          return userLogged;
+        }
       }
     }
   };
