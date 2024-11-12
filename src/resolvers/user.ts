2bc1593aadf9d47f88d18bd0ce083a14b981eb9a
Albin <alvinfer67@gmail.com>
2024-11-12 12:58:30 -0600

    update: hashing password in user register

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,7 +1,8 @@
 import { GraphQLError } from "graphql";
 import prisma from "../lib/prisma";
 import { decodeToken, generateToken } from "../services/token.service";
-import { authAsync, getRoles, isAuthorized } from "../utils/auth";
+import { authAsync } from "../utils/auth";
+import bcript from 'bcrypt'
 export const userResolvers = {
     Query: {
       users: authAsync(()=> {
@@ -17,7 +18,8 @@ export const userResolvers = {
         const userRole = await prisma.role.findFirst({ where: { name: "USER"}});
         const email = await prisma.user.findFirst({ where: { email: args.userInput.email}});
         if( email ) throw new GraphQLError('EMAIL_EXISTS');
-        const user = await prisma.user.create({ data: { roles: { connect: { id: userRole.id}}, ...args.userInput} , include:{ roles: true}});
+        const hashedPassword = bcript.hashSync(args.userInput.password, 10);
+        const user = await prisma.user.create({ data: { roles: { connect: { id: userRole.id}},...args.userInput, password: hashedPassword} , include:{ roles: true}});
         const roles = user.roles.map(role => role.name);
         const token = generateToken({...user,roles})
         res.cookie('restaurant_token',token , { httpOnly: true, maxAge: 1000 * 60 * 60 }); 
